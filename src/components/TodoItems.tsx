b3055df95a0f1ca2b134e321047ae453465c3788
Luciana <luciana.holzmann@gmail.com>
2024-05-28 18:33:41 +0200

    [FEAT] Estilos dinámicos

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,10 +1,13 @@
+// import IconCheck from "../assets/check.svg";
+// import IconError from "../assets/error.svg";
 
 const TodoItems = ({text, completed}: {text:string, completed:boolean}) => {
   return (
     <div className="containerTodoItem">
       <li className="liTodoItem">
-        <span className={`iconCheck iconCheckActive icon`}>V</span>
-        <p className={`textTodoItem textComplete`}>{text}</p>
+                              {/* CLASES DINAMICAS */}
+        <span className={`icon iconCheck ${completed && "iconCheckActive "}`}>V</span>
+        <p className={`textTodoItem ${completed && "textComplete"}`}>{text}</p>
         <span className="iconDelete icon">X</span>
       </li>
     </div>
