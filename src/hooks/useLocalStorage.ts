b0b3a158deb1f88895489b9d7023b5afa8379939
Luciana <luciana.holzmann@gmail.com>
2024-05-30 00:48:30 +0200

    [FEAT] Custom Hook: UseLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,24 +1,49 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 
-const UseLocalStorage = (itemName, initialValue) => {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parseItem;
+const UseLocalStorage = ({itemName, initialValue}: {itemName: string; initialValue: any[]}) => {
 
-  if (!localStorageItem) {
-    localStorage.getItem(itemName, JSON.stringify(initialValue));
-    parseItem = [];
-  } else {
-    parseItem = JSON.parse(localStorageItem);
-  }
+  const [item, setItem] = useState(initialValue);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  const [item, setItem] = useState(parseItem);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      try{
+        const localStorageItem = localStorage.getItem(itemName);
+        let parseItem;
+    
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+
+          parseItem = [];
+        } else {
+          parseItem = JSON.parse(localStorageItem);
+          setItem(parseItem);
+        }
+  
+          setLoading(false);
+        } catch(error){
+          setLoading(false);
+          setError(true);
+      }
+    }, 1000);
+
+  }, []);
 
   //ACTUALIZA ESTADO Y LOCALSTORAGE
-  const saveItem = (newItem) => {
+  const saveItem = (newItem: []) => {
     localStorage.setItem(itemName, JSON.stringify(newItem));
     setItem(newItem);
   };
-  return  [item, saveItem];
+
+
+  return  {
+    item, 
+    saveItem,
+    loading,
+    error};
 };
 
 export { UseLocalStorage };
